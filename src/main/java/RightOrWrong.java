1965bf76d27075898965cab0d80c2bc4b1813878
Anzhang2023 <youxin_0711@qq.com>
2024-09-23 22:55:40 +0800

    判断答案和问题是否正确，引用计算器未编写

---
+++ src/main/java/RightOrWrong.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.text.NumberFormat;
+
+/**
+ * @Author : ZhangYiXin
+ * @create 2024/9/23 21:25
+ */
+public class RightOrWrong {
+    //输入文件
+
+    public void RightWrong() {
+        //轮流读取文件的一行
+        //正则表达式判断格式是否正确
+        //计算题目答案
+        //比较
+        // 创建FileReader或InputStreamReader实例
+        try {
+            // 创建FileReader或InputStreamReader实例
+            FileReader fileReader1 = new FileReader("testExercises.txt");
+            FileReader fileReader2 = new FileReader("testAnswer.txt");
+            BufferedReader bufferedReader1 = new BufferedReader(fileReader1);
+            BufferedReader bufferedReader2 = new BufferedReader(fileReader2);
+            String line1;
+            String line2;
+            int rightNum = 0;
+            int wrongNum = 0;
+            StringBuffer right = new StringBuffer();
+            StringBuffer wrong = new StringBuffer();
+            String answer="1";
+            // 循环读取每一行
+            for (int i=1;(line1 = bufferedReader1.readLine()) != null&&(line2 = bufferedReader2.readLine()) != null;i++) {
+                System.out.println(line1);
+                String[] nums = line1.split(" ");
+                //计算器代码引用
+                if(answer==line2){
+                    rightNum++;
+                    right.append(i+" ");
+                }else{
+                    wrongNum++;
+                    wrong.append(i+" ");
+                }
+                System.out.println("Right: "+rightNum+" ( "+right+")");
+                System.out.println("Wrong: "+wrongNum+" ( "+wrong+")");
+            }
+            // 关闭流
+            bufferedReader1.close();
+            fileReader1.close();
+            bufferedReader2.close();
+            fileReader2.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/RightWriteTest.java
@@ -0,0 +1,10 @@
+/**
+ * @Author : ZhangYiXin
+ * @create 2024/9/23 22:16
+ */
+public class RightWriteTest {
+    public static void main(String[] args) {
+        RightOrWrong rightOrWrong = new RightOrWrong();
+        rightOrWrong.RightWrong();
+    }
+}
